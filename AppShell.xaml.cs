c456cd79b242a1989be3b8e7242b5a091a5b29f6
Indel <Indel@DESKTOP-BEC38OO>
2024-07-30 15:51:18 -0600

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _4955511BrochaGorda
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Models/PresupuestoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4955511BrochaGorda.Models
+{
+    partial class PresupuestoViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string metros;
+        [ObservableProperty]
+        public string precioxmetro;
+        [ObservableProperty]
+        public string total;
+
+        [RelayCommand]
+        private void Presupuesto()
+        {
+            if (double.TryParse(Metros, out double v1) && double.TryParse(Precioxmetro, out double v2))
+            {
+                Total = (v1 * v2).ToString("$0.00");
+            }
+            else
+            {
+                Total = "Ingrese números válidos";
+            }
+        }
+    }
+}
